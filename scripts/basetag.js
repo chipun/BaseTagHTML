5a8540ae7d17d7f571b9a8f17b82fb90e2f14509
Chi Ming Pun <chipun@gmail.com>
2016-05-11 19:36:39 -0400

    Modified a new plugin

--- scripts/basetag.js
+++ scripts/basetag.js
@@ -1,97 +1,113 @@
-var baseTag = (function() {
-    
-    var indexFile = (location.pathname.match(/\/(index[^\.]*\.html)/) || ['', ''])[1],
-        rUrl = /(#!\/|api|index[^\.]*\.html).*$/,
-        baseUrl = location.href.replace(rUrl, indexFile),
-        headEl = document.getElementsByTagName('head')[0],
-        sync = true;
+ 
+// create an functional expression to wrap our code
+ (function() {
 
-    // addTag('base', {
-    //     href: baseUrl
-    // });
-    // addTag('link', {
-    //     rel: 'stylesheet',
-    //     href: 'css/bootstrap.min.css',
-    //     type: 'text/css'
-    // });
-    // addTag('link', {
-    //     rel: 'stylesheet',
-    //     href: 'css/bootstrap.css',
-    //     type: 'text/css'
-    // });
+     var indexFile = (location.pathname.match(/\/(index[^\.]*\.html)/) || ['', ''])[1],
+         rUrl = /(#!\/|api|index[^\.]*\.html).*$/,
+         baseUrl = location.href.replace(rUrl, indexFile),
+         headEl = document.getElementsByTagName('head')[0],
+         sync = true;
 
-    // addTag('script', {
-    //     src: 'scripts/angular-1.5.5/angular.min.js'
-    // }, sync);
+    // define our constructor
+     this.baseTag = function() {
 
-    // addTag('script', {
-    //     src: 'scripts/angular-1.5.5/aa.select2.js'
-    // }, sync);
+         // Define option defaults 
+         var defaults = {
+             autoOpen: false,
+             className: 'fade-and-drop',
+             closeButton: true,
+             content: "",
+             maxWidth: 600,
+             minWidth: 280,
+             overlay: true
+         }
 
-    // addTag('script', {
-    //     src: 'scripts/bootstrap.js'
-    // }, sync);
-    // addTag('script', {
-    //     src: 'scripts/bootstrap.min.js'
-    // }, sync);
+     }
 
-    var setupTags = function(list) {
-        console.log("Start looping...");
+     //Public Methods
+     baseTag.prototype.addTags = function(list) {
+
+        if(!Array.isArray(list))
+        {
+            throw "The parameter value is not an Array";
+
+        }
 
          for (var item in list) {
 
-            var keyName = Object.keys(list[item])[0];
+             var keyName = Object.keys(list[item])[0];
 
-            switch(keyName)
-            {
-                case "script":
+             switch (keyName) {
+                 case "script":
                      addTag(keyName, list[item].script, sync);
-                break;
+                     break;
 
-                case "link":
+                 case "link":
                      addTag(keyName, list[item].link, sync);
-                break;
-            };
-            
-            console.log(list[item]);
-        }
+                     break;
+             };
+         }
 
-        console.log("End looping...")
-    }
+     }
+     //End Public Methods
 
+    // Private Methods
+     function addTag(name, attributes, sync) {
+         var el = document.createElement(name),
+             attrName;
 
-    function addTag(name, attributes, sync) {
-        var el = document.createElement(name),
-            attrName;
+         for (attrName in attributes) {
+             el.setAttribute(attrName, attributes[attrName]);
+         }
 
-        for (attrName in attributes) {
-            el.setAttribute(attrName, attributes[attrName]);
-        }
+         sync ? document.write(outerHTML(el)) : headEl.appendChild(el);
+     }
 
-        sync ? document.write(outerHTML(el)) : headEl.appendChild(el);
-    }
+     function outerHTML(node) {
+         // if IE, Chrome take the internal method otherwise build one
+         return node.outerHTML || (
+             function(n) {
+                 var div = document.createElement('div'),
+                     h;
+                 div.appendChild(n);
+                 h = div.innerHTML;
+                 div = null;
+                 return h;
+             })(node);
+     }
 
-    function outerHTML(node) {
-        // if IE, Chrome take the internal method otherwise build one
-        return node.outerHTML || (
-            function(n) {
-                var div = document.createElement('div'),
-                    h;
-                div.appendChild(n);
-                h = div.innerHTML;
-                div = null;
-                return h;
-            })(node);
-    }
-
-    return {
-
-        init: function(s) {
-            setupTags(s);
-        }
-
-    }
+     //END Private Methods
 
 
+          // addTag('base', {
+     //     href: baseUrl
+     // });
+     // addTag('link', {
+     //     rel: 'stylesheet',
+     //     href: 'css/bootstrap.min.css',
+     //     type: 'text/css'
+     // });
+     // addTag('link', {
+     //     rel: 'stylesheet',
+     //     href: 'css/bootstrap.css',
+     //     type: 'text/css'
+     // });
 
-})();
+     // addTag('script', {
+     //     src: 'scripts/angular-1.5.5/angular.min.js'
+     // }, sync);
+
+     // addTag('script', {
+     //     src: 'scripts/angular-1.5.5/aa.select2.js'
+     // }, sync);
+
+     // addTag('script', {
+     //     src: 'scripts/bootstrap.js'
+     // }, sync);
+     // addTag('script', {
+     //     src: 'scripts/bootstrap.min.js'
+     // }, sync);
+
+
+
+ })();
